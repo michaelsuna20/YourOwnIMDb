75915127177866fd47572858af140ec3b36b57c2
michaelsuna20 <michaelsuna20@gmail.com>
2025-02-12 18:57:01 -0500

    problem 1

--- app/routes/actors.py
+++ app/routes/actors.py
@@ -11,7 +11,7 @@ def view_all_actors():
     # >>>> TODO 1: Write a query that fetches all actors from `People` <<<<
     #              The query should retrieve `name`, `nationality`, `dob`, and `gender`.
 
-    query = """ """
+    query = "SELECT P.name, P.nationality, P.dob, P.gender FROM People P JOIN Role R ON P.id = R.pid WHERE R.role_name = 'Actor';"
 
     with Database() as db:
         actors = db.execute(query=query)
